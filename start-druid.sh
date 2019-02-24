f26e0842bba56008a67e954657335ad2c8227f63
Khwunchai J <khwunchai.j@ku.th>
2019-02-24 20:10:27 +0700

    Use FQDN in druid.host

--- start-druid.sh
+++ start-druid.sh
@@ -9,8 +9,6 @@ fi
 
 DB_CONNECT_URI="jdbc:${DB_TYPE}\:\/\/${DB_HOST}\:${DB_PORT}\/${DB_DBNAME}"
 
-sed -ri 's/druid.host=.*/druid.host='$(hostname)'/g' /opt/druid/conf/druid/$1/runtime.properties
-
 sed -ri 's#druid.zk.service.host.*#druid.zk.service.host='${ZOOKEEPER_HOST}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
 sed -ri 's#druid.metadata.storage.type.*#druid.metadata.storage.type='${DB_TYPE}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
 sed -ri 's#druid.metadata.storage.connector.connectURI.*#druid.metadata.storage.connector.connectURI='${DB_CONNECT_URI}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
@@ -24,28 +22,26 @@ if [ "$STORAGE_TYPE" == "s3" ]; then
     sed -ri 's#druid.storage.bucket.*#druid.storage.bucket='${S3_STORAGE_BUCKET}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
     sed -ri 's#druid.storage.baseKey.*#druid.storage.baseKey='${S3_STORAGE_BASE_KEY}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
     # sed -ri 's#druid.indexer.logs.s3Bucket.*#druid.indexer.logs.s3Bucket='${S3_INDEXING_BUCKET}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
+elif [ "$DRUID_DEEPSTORAGE_LOCAL_DIR" != "-" ]; then
+    sed -ri 's/druid.storage.storageDirectory=.*/druid.storage.storageDirectory='${DRUID_DEEPSTORAGE_LOCAL_DIR}'/g' /opt/druid/conf/druid/_common/common.runtime.properties
 fi
 
-if [ "$DRUID_HOSTNAME" != "-" ]; then
+if [ "$DRUID_USE_CONTAINER_IP" != "-" ]; then
+    ipaddress=`ip a|grep "global eth0"|awk '{print $2}'|awk -F '\/' '{print $1}'`
+    sed -ri 's/druid.host=.*/druid.host='${ipaddress}'/g' /opt/druid/conf/druid/$1/runtime.properties
+elif [ "$DRUID_HOSTNAME" != "-" ]; then
     sed -ri 's/druid.host=.*/druid.host='${DRUID_HOSTNAME}'/g' /opt/druid/conf/druid/$1/runtime.properties
+else
+    sed -ri 's/druid.host=.*/druid.host='$(hostname -f)'/g' /opt/druid/conf/druid/$1/runtime.properties
 fi
 
 if [ "$DRUID_LOGLEVEL" != "-" ]; then
     sed -ri 's/druid.emitter.logging.logLevel=.*/druid.emitter.logging.logLevel='${DRUID_LOGLEVEL}'/g' /opt/druid/conf/druid/_common/common.runtime.properties
 fi
 
-if [ "$DRUID_USE_CONTAINER_IP" != "-" ]; then
-    ipaddress=`ip a|grep "global eth0"|awk '{print $2}'|awk -F '\/' '{print $1}'`
-    sed -ri 's/druid.host=.*/druid.host='${ipaddress}'/g' /opt/druid/conf/druid/$1/runtime.properties
-fi
-
 if [ "$DRUID_SEGMENTCACHE_LOCATION" != "-" ]; then
     sed -ri 's/druid.segmentCache.locations=[{"path":*,"maxSize"\:100000000000}]/druid.segmentCache.locations=[{"path":'${DRUID_SEGMENTCACHE_LOCATION}',"maxSize"\:100000000000}]/g' /opt/druid/conf/druid/$1/runtime.properties
     sed -ri 's/var\/druid\/segment-cache/'${DRUID_SEGMENTCACHE_LOCATION}'/g' /opt/druid/conf/druid/$1/runtime.properties
-fi
-
-if [ "$DRUID_DEEPSTORAGE_LOCAL_DIR" != "-" ]; then
-    sed -ri 's/druid.storage.storageDirectory=.*/druid.storage.storageDirectory='${DRUID_DEEPSTORAGE_LOCAL_DIR}'/g' /opt/druid/conf/druid/_common/common.runtime.properties
 fi
 
 java ${JAVA_OPTS} -cp /opt/druid/conf/druid/_common:/opt/druid/conf/druid/$1:/opt/druid/lib/* io.druid.cli.Main server $@
