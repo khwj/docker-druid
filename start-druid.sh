60c3b29116935bf6cf7362b49f012bda868d17c3
Khwunchai J <khwunchai.j@ku.th>
2019-03-01 08:31:52 +0700

    Typo

--- start-druid.sh
+++ start-druid.sh
@@ -26,11 +26,11 @@ if [ "$DRUID_S3_SECRET_KEY" != "" ]; then
     sed -ri 's#druid.s3.secretKey.*#druid.s3.secretKey='${DRUID_S3_SECRET_KEY}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
 fi
 
-if [ "$DRUID_S3_ACCESS_KEY" == "" ]; then
+if [ "$DRUID_STORAGE_TYPE" != "" ]; then
     sed -ri 's/druid.storage.type.*/druid.storage.type='${DRUID_STORAGE_TYPE}'/g' /opt/druid/conf/druid/_common/common.runtime.properties
     if [ "$DRUID_STORAGE_TYPE" == "s3" ]; then
-        sed -ri 's#druid.storage.bucket.*#druid.storage.bucket='${DRUID_S3_STORAGE_BUCKET}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
-        sed -ri 's#druid.storage.baseKey.*#druid.storage.baseKey='${DRUID_S3_STORAGE_BASE_KEY}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
+        sed -ri 's#druid.storage.bucket.*#druid.storage.bucket='${DRUID_STORAGE_BUCKET}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
+        sed -ri 's#druid.storage.baseKey.*#druid.storage.baseKey='${DRUID_STORAGE_BASE_KEY}'#g' /opt/druid/conf/druid/_common/common.runtime.properties
     fi
     if [ "$DRUID_DEEPSTORAGE_LOCAL_DIR" != "-" ]; then
         sed -ri 's/druid.storage.storageDirectory=.*/druid.storage.storageDirectory='${DRUID_DEEPSTORAGE_LOCAL_DIR}'/g' /opt/druid/conf/druid/_common/common.runtime.properties
